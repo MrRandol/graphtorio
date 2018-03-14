adeff30a88679994d2d394bfc55f1318593a5174
MrRandol <maxime.choffat@gmail.com>
2018-03-14 15:10:07 +0100

    add tests for GET id & PUT items

--- graphtorio-api/src/main/test/item/itemController.js
+++ graphtorio-api/src/main/test/item/itemController.js
@@ -48,6 +48,23 @@ describe('ITEM CONTROLLER', function() {
     })
   });
 
+  it('should return item /items/:id GET', function(done) {
+    let item = factories.validItem();
+    Item.create(item)
+    .then((item) => {
+      chai.request(server)
+      .get('/items/' + item.id)
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.name.should.equal(item.name);
+        res.body.label.should.equal(item.label);
+        done();
+      });
+    })
+  });
+
   it('should add a SINGLE item on /items POST', function(done) {
     let item = factories.validItem();
     chai.request(server)
@@ -68,4 +85,30 @@ describe('ITEM CONTROLLER', function() {
     });
   });
   
+  it('should update an item on /items PUT', function(done) {
+    let it = factories.validItem()
+    Item.create(it)
+    .then(
+      (item) => {
+        return Item.findByIdAndUpdate(item.id, {name: "newName"}); 
+      })
+    .then((item) => {
+      chai.request(server)
+      .get('/items/' + item.id)
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+
+        res.body.should.have.property('name');
+        res.body.should.have.property('label');
+        res.body.should.have.property('_id');
+
+        res.body.name.should.equal("newName");
+        res.body.label.should.equal(item.label);
+        done();
+      });
+    })
+  });
+
 });
